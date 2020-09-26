310f442bc6f955b2a0d4f07db99188759fef58fe
noel <noel.chiocchetti@ch.ibm.com>
2020-09-26 14:36:11 +0200

    Initial commit for our Spring Boot Reddit clone

---
+++ src/test/java/com/chiocchetti/springit/SpringitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chiocchetti.springit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
