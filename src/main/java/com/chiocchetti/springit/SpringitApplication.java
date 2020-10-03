8ccbb1fdae7817a74c6f068a54f81f700f03dc82
noel <noel.chiocchetti@ch.ibm.com>
2020-10-03 12:36:31 +0200

    Created entities and refactored using Project Lombok

--- src/main/java/com/chiocchetti/springit/SpringitApplication.java
+++ src/main/java/com/chiocchetti/springit/SpringitApplication.java
@@ -1,13 +1,30 @@
 package com.chiocchetti.springit;
 
+import com.chiocchetti.springit.config.SpringitProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(SpringitProperties.class)
 public class SpringitApplication {
+
+	@Autowired
+	private SpringitProperties springitProperties;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringitApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner runner () {
+		return args -> {
+			System.out.println("Welcome message: " + springitProperties.getWelcomeMsg());
+		};
 	}
 
 }
